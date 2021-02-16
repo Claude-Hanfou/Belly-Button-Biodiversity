5c58db7802b6d9208d15f378be1becc8ad592e3f
Claude-Hanfou <claudehanfou@gmail.comgit config --global user.name Claude-Hanfougit config --global user.email you@example.com  git config --global user.name Your>
2021-02-15 20:50:17 -0600

    update

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,70 +1,177 @@
 // Begining of the code below
 
 
+//Append names to the dropdown by reading the json file and appending it
+function init() {
+  var selector = d3.select("#selDataset");
 
-// Create a function to unpact the json data
-
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    console.log(sampleNames);
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
     });
-  }
+})}
 
+init();
 
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildBarChart(newSample);
 
-// Start by reading the json file 
+} 
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    console.log(resultArray);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
 
-fetch("samples.json")
-    .then(function(resp) {
-        return resp.json();
+    PANEL.html("");
+    
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(key.toUpperCase() + ': ' + value); 
     })
 
-    .then(result => {
-        jsonFileData = result;
-      });
+
+  });
+}
 
 
-console.log(jsonFileData)
 
-function buildPlot() {
+
+
+// // Start by reading the json file 
+
+
+
+
+
+url = "../../../data/samples.json"
+
+
+
+
+// function buildBarChart(sample) {
+//   d3.json("samples.json").then((data) => {
+//     var resultArray = data
+//     .samples
+//     .filter(sampleObj => {
+//       return sampleObj.id == sample
+//     });
+
     
- 
-
-       // Grab values from the data json object to build the plots
-        //   var sample = data.samples;
 
 
-        //   var stock = data.dataset.dataset_code;
+
+
+//     // Grab values from the data json object to build the plots
+    
+//     // var sampleValues = data.samples;
+//     // console.log(sampleValues)
+
+//     // var samplesResult = sampleValues.filter(sampleVal => {return sampleVal.id == sample})
+//     // console.log(samplesResult)
+
+//     // var result = metadata.filter(sampleObj => sampleObj.id == sample);
+    
+   
+    
+
+     
+
+
+
+
+
+    
+// //     // var otuId = data['samples'][0]['otu_ids']
+// //     // console.log(otuId)
+
+// //     // var text = data['samples'][0]['otu_labels']
+// //     // console.log(text)
+
+    
+
+
+//     console.log(data)
+
+   
+// //           // var sample = data.samples;
+
+
+// //           // var stock = data.dataset.dataset_code;
           
       
-        //   console.log(data)
+// //           // console.log(data)
       
-        //   var trace1 = {
-        //     type: "barer",
-        //     x: 
-        //     y: 
-        //     orientation: "h"
-        //   };
+// //           // var trace1 = {
+// //           //   type: "barer",
+// //           //   x: 
+// //           //   y: 
+// //           //   orientation: "h"
+// //           // };
       
-        //   var data = [trace1];
+// //           // var data = [trace1];
 
-        //   var layout = {
-        //     title: "Bar",
-        //     margin: {
-        //       l: 100,
-        //       r: 100,
-        //       t: 100,
-        //       b: 100
-        //     }
-        //   };
+// //           // var layout = {
+// //           //   title: "Bar",
+// //           //   margin: {
+// //           //     l: 100,
+// //           //     r: 100,
+// //           //     t: 100,
+// //           //     b: 100
+// //           //   }
+// //           // };
       
-        //   Plotly.newPlot("plot", data, layout);
+// //           // Plotly.newPlot("plot", data, layout);
       
-        }
-      
-      
-      buildPlot(); 
-
+//   // });
+// }
+// buildPlot ();
 
 
+function buildBarChart(sample) {
+  d3.json("samples.json").then((data) => {
+    var resultArray = data
+    .samples
+    .filter(sampleObj => {
+      return sampleObj.id == sample
+    });
+    
+    var result = resultArray[0];
+    console.log(result);
+    var top_ten_otu_ids = result.otu_ids.slice(0, 10).map(numericIds => {
+      return 'OTU ' + numericIds;
+    }).reverse();
+    
+    var top_ten_sample_values = result.sample_values.slice(0, 10).reverse();
+    var top_ten_otu_labels = result.otu_labels.slice(0, 10).reverse();
+    
+    var bar_trace = [
+      {
+        x: top_ten_sample_values,  
+        y: top_ten_otu_ids,
+        text: top_ten_otu_labels,
+        name: "Top 10",
+        type: 'bar',
+        orientation: 'h'
+      }
+      ];
 
+      var data = [bar_trace];
+
+      var bar_layout = {
+        title: "Top 10 OTUs",
+   
+      };
+      
+      Plotly.newPlot('bar', bar_trace, bar_layout)
+    
+    });
+  }
