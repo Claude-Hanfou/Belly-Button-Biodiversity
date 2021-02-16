511847cab20142cf11ada5bd88fea11a3890bff3
Claude-Hanfou <claudehanfou@gmail.comgit config --global user.name Claude-Hanfougit config --global user.email you@example.com  git config --global user.name Your>
2021-02-15 21:23:52 -0600

    update

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -29,15 +29,16 @@ function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    console.log(resultArray);
     var result = resultArray[0];
     var PANEL = d3.select("#sample-metadata");
 
     PANEL.html("");
     
     Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(key.toUpperCase() + ': ' + value); 
+      PANEL.append("h6").text(key.toLowerCase() + ': ' + value); 
     })
+
+    console.log(result);
 
 
   });
