cd5c2dee6740dd14bb6ef36a70fc39b6ab489c76
Claude-Hanfou <claudehanfou@gmail.comgit config --global user.name Claude-Hanfougit config --global user.email you@example.com  git config --global user.name Your>
2021-02-21 18:35:56 -0600

    update

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ init();
 function optionChanged(newSample) {
   buildMetadata(newSample);
   buildBarChart(newSample);
-  buildGaugeChart(newSample);
+  buildGauge(newSample);
   buildBubbleChart(newSample);
 
 } 
@@ -123,55 +123,177 @@ function buildBarChart(sample) {
 
 //build the gauge plotly
 
-function buildGaugeChart(sample) {
-  d3.json("samples.json").then((data) => {
+// function buildGaugeChart(sample) {
+//   d3.json("samples.json").then((data) => {
 
-    var frequency = data.metadata
-    console.log(frequency)
+//     var frequency = data.metadata
+//     console.log(frequency)
 
-    var freqResult = frequency.filter(freq => freq.id == sample)
-    var results = freqResult[0]
-    console.log(results)
+//     var freqResult = frequency.filter(freq => freq.id == sample)
+//     var results = freqResult[0]
+//     console.log(results)
 
-    var freqScrub = results.wfreq
+//     var freqScrub = results.wfreq
 
-    var dataGauge = [
-      {
-        domain: { x: [0, 1], y: [0, 1] },
+//     var dataGauge = [
+//       {
+//         domain: { x: [0, 1], y: [0, 1] },
 
-        value: freqScrub,
-        title: { text: "Scrubs Per Week" },
-        type: "indicator",
-        mode: "gauge+number",
+//         value: freqScrub,
+//         title: { text: "Scrubs Per Week" },
+//         type: "indicator",
+//         mode: "gauge+number",
 
-        gauge: {
-          axis: { range: [null, 10] },
-          bar: { color: "salmon" },
-          steps: [
-            { range: [0, 1], color: 'rgba(14, 127, 0, .5)'},
-            { range: [1,2], color: 'rgba(110, 154, 22, .5)'},
-            { range: [2,3], color: 'rgba(170, 202, 42, .5)' },
-            { range: [3, 4], color: 'rgba(202, 209, 95, .5)'},
-            { range: [4, 5], color: 'rgba(210, 206, 145, .5)' },
-            { range: [5, 6], color: 'rgba(232, 226, 202, .5)' },
-            { range: [6, 7], color: 'rgba(255, 255, 255, 0)'},
-            { range: [7, 8], color: "lightgreen" },
-            { range: [8, 9], color: "green" },
-            { range: [9, 10], color: "teal" }
-
-          ]},
-      }
-    ];
+//         gauge: {
+//           axis: { range: [null, 10] },
+//           bar: { color: "salmon" },
+//           {values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50,9],
+//           rotation: 90,
+//           text: ['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9','9-10'],
+//           textinfo: 'text',
+//           textposition:'inside',	  
+//           marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+//                          'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+//                          'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+//                          'rgba(255, 255, 255, 0)']},
+//           labels: ['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9','9-10'],
+//           hoverinfo: 'label',
+//           hole: .5,
+//           type: 'pie',
+//           showlegend: false }
+            
+         
+//           }
+//       }]
     
-    var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-    Plotly.newPlot('gauge', dataGauge, layout);
+//     var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+//     Plotly.newPlot('gauge', dataGauge, layout);
  
 
 
-  })
+//    })
+
+// }
 
 
+
+
+
+// text: ['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9','9-10'],
+//           textinfo: 'text',
+//           textposition:'inside',	
+//           steps: [
+//             { range: [0, 1], color: 'rgba(14, 127, 0, .5)'},
+//             { range: [1,2], color: 'rgba(110, 154, 22, .5)'},
+//             { range: [2,3], color: 'rgba(170, 202, 42, .5)' },
+//             { range: [3, 4], color: 'rgba(202, 209, 95, .5)'},
+//             { range: [4, 5], color: 'rgba(210, 206, 145, .5)' },
+//             { range: [5, 6], color: 'rgba(232, 226, 202, .5)' },
+//             { range: [6, 7], color: 'rgba(255, 255, 255, 0)'},
+//             { range: [7, 8], color: "lightgreen" },
+//             { range: [8, 9], color: "green" },
+//             { range: [9, 10], color: "teal" }
+
+//           ]},
+
+
+
+function buildGauge(wfreq) {
+  // Enter the washing frequency between 0 and 180
+  var level = parseFloat(wfreq) * 20;
+
+  // Trig to calc meter point
+  var degrees = 180 - level;
+  var radius = 0.5;
+  var radians = (degrees * Math.PI) / 180;
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  // Path: may have to change to create a better triangle
+  var mainPath = "M -.0 -0.05 L .0 0.05 L ";
+  var pathX = String(x);
+  var space = " ";
+  var pathY = String(y);
+  var pathEnd = " Z";
+  var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+  var data = [
+    {
+      type: "scatter",
+      x: [0],
+      y: [0],
+      marker: { size: 12, color: "850000" },
+      showlegend: false,
+      name: "Freq",
+      text: level,
+      hoverinfo: "text+name"
+    },
+    {
+      values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+      rotation: 90,
+      text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      textinfo: "text",
+      textposition: "inside",
+      marker: {
+        colors: [
+          "rgba(0, 105, 11, .5)",
+          "rgba(10, 120, 22, .5)",
+          "rgba(14, 127, 0, .5)",
+          "rgba(110, 154, 22, .5)",
+          "rgba(170, 202, 42, .5)",
+          "rgba(202, 209, 95, .5)",
+          "rgba(210, 206, 145, .5)",
+          "rgba(232, 226, 202, .5)",
+          "rgba(240, 230, 215, .5)",
+          "rgba(255, 255, 255, 0)"
+        ]
+      },
+      labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      hoverinfo: "label",
+      hole: 0.5,
+      type: "pie",
+      showlegend: false
+    }
+  ];
+
+  var layout = {
+    shapes: [
+      {
+        type: "path",
+        path: path,
+        fillcolor: "850000",
+        line: {
+          color: "850000"
+        }
+      }
+    ],
+    title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+    height: 500,
+    width: 500,
+    xaxis: {
+      zeroline: false,
+      showticklabels: false,
+      showgrid: false,
+      range: [-1, 1]
+    },
+    yaxis: {
+      zeroline: false,
+      showticklabels: false,
+      showgrid: false,
+      range: [-1, 1]
+    }
+  };
+
+  var GAUGE = document.getElementById("gauge");
+  Plotly.newPlot(GAUGE, data, layout);
 }
+
+
+
+
+
+
+
 
 
 
